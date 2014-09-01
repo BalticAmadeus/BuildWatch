c46216e31fc73eb28da57bc209b0712def6b5ab5
Antanas Kompanas <akompanas@users.noreply.github.com>
2014-09-01 21:49:02 +0300

    Added log4net initialization to Windows Service component.

--- DataSource.Service/DataSourceService.cs
+++ DataSource.Service/DataSourceService.cs
@@ -8,10 +8,14 @@ using System.ServiceProcess;
 using System.Text;
 using BuildWatch.DataSource.Common;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace BuildWatch.DataSource.Service
 {
     public partial class DataSourceService : ServiceBase
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(DataSourceService));
+
         private DataSourceManager _dsman;
 
         public DataSourceService()
@@ -21,6 +25,7 @@ namespace BuildWatch.DataSource.Service
 
         protected override void OnStart(string[] args)
         {
+            log.Info("Starting BuildWatch as a service");
             _dsman = new DataSourceManager();
             _dsman.Initialize(new Type[] { typeof(BuildWatch.DataSource.TFS.TFSDataSource) } );
             _dsman.Start();
@@ -28,6 +33,7 @@ namespace BuildWatch.DataSource.Service
 
         protected override void OnStop()
         {
+            log.Info("Stopping BuildWatch service");
             _dsman.Stop();
         }
     }
