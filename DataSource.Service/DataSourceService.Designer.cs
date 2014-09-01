85363d91f00b2ea582448fb2b97fe92fa53e434e
Antanas Kompanas <akompanas@users.noreply.github.com>
2014-09-01 21:49:32 +0300

    Added x64 build target and Windows Service component installer.

--- DataSource.Service/DataSourceService.Designer.cs
+++ DataSource.Service/DataSourceService.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            // 
+            // DataSourceService
+            // 
+            this.ServiceName = "BuildWatchTFSService";
+
         }
 
         #endregion
