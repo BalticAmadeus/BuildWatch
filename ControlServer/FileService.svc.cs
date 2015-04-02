dfdddd46c97cf0ba9a1c3fef45ca972e3ebe1985
Antanas Kompanas <akompanas@users.noreply.github.com>
2015-04-02 14:30:04 +0300

    Introduced file service for streaming content such as images.

---
+++ ControlServer/FileService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BuildWatch.ControlServer
+{
+    public class FileService : IFileService
+    {
+        public GetPictureResp GetPicture(GetPictureReq req)
+        {
+            var resp = new GetPictureResp();
+            PictureCacheEntry entry = AppContext.Current.GetUserPicture(req.UserName);
+            resp.PictureHash = entry.Hash;
+            resp.PictureData = new MemoryStream(entry.Data);
+            return resp;
+        }
+    }
+}
---
+++ ControlServer/IFileService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BuildWatch.ControlServer
+{
+    [ServiceContract]
+    public interface IFileService
+    {
+        [OperationContract]
+        GetPictureResp GetPicture(GetPictureReq req);
+    }
+
+    #region GetPicture
+
+    [MessageContract]
+    public class GetPictureReq
+    {
+        [MessageBodyMember]
+        public string UserName;
+    }
+
+    [MessageContract]
+    public class GetPictureResp
+    {
+        [MessageHeader]
+        public string PictureHash;
+
+        [MessageBodyMember]
+        public Stream PictureData;
+    }
+
+    #endregion
+}
